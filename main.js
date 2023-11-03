38c62d36e8d4db32588301f0565773751a67c013
MariMari021 <mariana.gsilva0021@gmail.com>
2023-11-02 23:35:22 -0300

    okok
    okok

--- main.js
+++ main.js
@@ -269,3 +269,28 @@ document.getElementById('prev').onclick = function(){
 
 //produtos
 AOS.init();
+//fim produtos
+
+//ScrollTrigger
+const container = document.getElementById('scroll-container');
+const spans = container.querySelectorAll('span');
+let currentIndex = 0;
+
+container.addEventListener('wheel', (e) => {
+  container.style.overflow = 'hidden';
+
+  if (e.deltaY > 0) {
+    currentIndex = Math.min(currentIndex + 1, spans.length - 1);
+  } else {
+    currentIndex = Math.max(currentIndex - 1, 0);
+  }
+
+  spans.forEach((span, index) => {
+    if (index === currentIndex) {
+      span.style.display = 'inline';
+    } else {
+      span.style.display = 'none';
+    }
+  });
+});
+//fim ScrollTrigger
